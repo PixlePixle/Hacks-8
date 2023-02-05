9f1a255bd394efa357e65d5605829c338e0a418f
husky3802 <nfollo7@gmail.com>
2023-02-05 00:04:05 -0500

    Fixed the x-axis camera and added offset to mimic other games of the genre

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -13,8 +13,9 @@ public class CameraFollow : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 targetPosition = new Vector3(target.position.x + offset.x, target.position.y + offset.y, transform.position.z);
+        Vector3 targetPosition = new Vector3(0, target.position.y + offset.y, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
+        transform.position = new Vector3(target.position.x + offset.x, transform.position.y, transform.position.z);
     }
 }
 
